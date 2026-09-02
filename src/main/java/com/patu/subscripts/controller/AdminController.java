880a7caa0bdddb59a9811bd539ca99b15b12b473
Nazmul <afnannazmul@gmail.com>
2026-09-02 17:28:02 +0600

    enhance user management: add self-block prevention and improve UI layout

--- src/main/java/com/patu/subscripts/controller/AdminController.java
+++ src/main/java/com/patu/subscripts/controller/AdminController.java
@@ -1,6 +1,8 @@
 package com.patu.subscripts.controller;
 
 
+import com.patu.subscripts.model.User;
+import com.patu.subscripts.repository.UserRepository;
 import com.patu.subscripts.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -9,11 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Controller
 @RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminController {
-
+    private final UserRepository userRepository;
     private final UserService userService;
 
 
@@ -24,8 +28,18 @@ public class AdminController {
     }
 
     @GetMapping("/block/{id}")
-    public String handleBlock(@PathVariable int id) {
+    public String handleBlock(@PathVariable int id, Principal principal) {
+        String loggedInEmail = principal.getName();
+
+
+        User admin = userRepository.findByEmail(loggedInEmail).get();
+
+
+        if (admin.getId() == id) {
+            return "redirect:/admin/users?error=selfblock";
+        }
         userService.toggleUserBlock(id);
+
         return "redirect:/admin/users";
     }
 }
